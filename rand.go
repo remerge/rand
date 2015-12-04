4805b25eceb9adedce9bb91d514853e672a0f074
Benedikt Böhm <bb@xnull.de>
2015-12-04 11:21:23 +0100

    complete api

--- rand.go
+++ rand.go
@@ -26,6 +26,11 @@ func init() {
 	}
 }
 
+func Next() *Xor128Rand {
+	apos := int(atomic.AddUint64(&pos, 1) % poolSize)
+	return pool[apos]
+}
+
 type Xor128Rand struct {
 	src [2]uint64
 }
@@ -45,41 +50,86 @@ func (r *Xor128Rand) Uint64() uint64 {
 }
 
 func Uint64() uint64 {
-	apos := int(atomic.AddUint64(&pos, 1) % poolSize)
-	return pool[apos].Uint64()
+	return Next().Uint64()
 }
 
 // Int63 returns a non-negative pseudo-random 63-bit integer as an int64.
-func (r *Xor128Rand) Int63() int64 { return int64(r.Uint64()) & mask }
+func (r *Xor128Rand) Int63() int64 {
+	return int64(r.Uint64()) & mask
+}
 
 // Int63 returns a non-negative pseudo-random 63-bit integer as an int64.
-func Int63() int64 { return int64(Uint64()) & mask }
+func Int63() int64 {
+	return Next().Int63()
+}
 
 // Uint32 returns a pseudo-random 32-bit value as a uint32.
-func Uint32() uint32 { return uint32(Int63() >> 31) }
+func (r *Xor128Rand) Uint32() uint32 {
+	return uint32(r.Int63() >> 31)
+}
+
+// Uint32 returns a pseudo-random 32-bit value as a uint32.
+func Uint32() uint32 {
+	return Next().Uint32()
+}
 
 // Int31 returns a non-negative pseudo-random 31-bit integer as an int32.
-func Int31() int32 { return int32(Int63() >> 32) }
+func (r *Xor128Rand) Int31() int32 {
+	return int32(r.Int63() >> 32)
+}
+
+// Int31 returns a non-negative pseudo-random 31-bit integer as an int32.
+func Int31() int32 {
+	return Next().Int31()
+}
+
+// Int returns a non-negative pseudo-random int.
+func (r *Xor128Rand) Int() int {
+	u := uint(r.Int63())
+	return int(u << 1 >> 1) // clear sign bit if int == int32
+}
 
 // Int returns a non-negative pseudo-random int.
 func Int() int {
-	u := uint(Int63())
-	return int(u << 1 >> 1) // clear sign bit if int == int32
+	return Next().Int()
+}
+
+// Int63n returns, as an int64, a non-negative pseudo-random number in [0,n).
+// It panics if n <= 0.
+func (r *Xor128Rand) Int63n(n int64) int64 {
+	if n <= 0 {
+		panic("invalid argument to Int63n")
+	}
+	if n&(n-1) == 0 { // n is power of two, can mask
+		return r.Int63() & (n - 1)
+	}
+	max := int64((1 << 63) - 1 - (1<<63)%uint64(n))
+	v := r.Int63()
+	for v > max {
+		v = r.Int63()
+	}
+	return v % n
 }
 
 // Int63n returns, as an int64, a non-negative pseudo-random number in [0,n).
 // It panics if n <= 0.
 func Int63n(n int64) int64 {
+	return Next().Int63n(n)
+}
+
+// Int31n returns, as an int32, a non-negative pseudo-random number in [0,n).
+// It panics if n <= 0.
+func (r *Xor128Rand) Int31n(n int32) int32 {
 	if n <= 0 {
-		panic("invalid argument to Int63n")
+		panic("invalid argument to Int31n")
 	}
 	if n&(n-1) == 0 { // n is power of two, can mask
-		return Int63() & (n - 1)
+		return r.Int31() & (n - 1)
 	}
-	max := int64((1 << 63) - 1 - (1<<63)%uint64(n))
-	v := Int63()
+	max := int32((1 << 31) - 1 - (1<<31)%uint32(n))
+	v := r.Int31()
 	for v > max {
-		v = Int63()
+		v = r.Int31()
 	}
 	return v % n
 }
@@ -87,34 +137,29 @@ func Int63n(n int64) int64 {
 // Int31n returns, as an int32, a non-negative pseudo-random number in [0,n).
 // It panics if n <= 0.
 func Int31n(n int32) int32 {
+	return Next().Int31n(n)
+}
+
+// Intn returns, as an int, a non-negative pseudo-random number in [0,n).
+// It panics if n <= 0.
+func (r *Xor128Rand) Intn(n int) int {
 	if n <= 0 {
-		panic("invalid argument to Int31n")
+		panic("invalid argument to Intn")
 	}
-	if n&(n-1) == 0 { // n is power of two, can mask
-		return Int31() & (n - 1)
+	if n <= 1<<31-1 {
+		return int(r.Int31n(int32(n)))
 	}
-	max := int32((1 << 31) - 1 - (1<<31)%uint32(n))
-	v := Int31()
-	for v > max {
-		v = Int31()
-	}
-	return v % n
+	return int(r.Int63n(int64(n)))
 }
 
 // Intn returns, as an int, a non-negative pseudo-random number in [0,n).
 // It panics if n <= 0.
 func Intn(n int) int {
-	if n <= 0 {
-		panic("invalid argument to Intn")
-	}
-	if n <= 1<<31-1 {
-		return int(Int31n(int32(n)))
-	}
-	return int(Int63n(int64(n)))
+	return Next().Intn(n)
 }
 
 // Float64 returns, as a float64, a pseudo-random number in [0.0,1.0).
-func Float64() float64 {
+func (r *Xor128Rand) Float64() float64 {
 	// A clearer, simpler implementation would be:
 	//	return float64(Int63n(1<<53)) / (1<<53)
 	// However, Go 1 shipped with
@@ -133,22 +178,31 @@ func Float64() float64 {
 	// and those steal from what would normally be 0 results.)
 	// The remapping only happens 1/2⁵³ of the time, so most clients
 	// will not observe it anyway.
-	f := float64(Int63()) / (1 << 63)
+	f := float64(r.Int63()) / (1 << 63)
 	if f == 1 {
 		f = 0
 	}
 	return f
 }
 
-func Float64Range(a, b float64) float64 {
+// Float64 returns, as a float64, a pseudo-random number in [0.0,1.0).
+func Float64() float64 {
+	return Next().Float64()
+}
+
+func (r *Xor128Rand) Float64Range(a, b float64) float64 {
 	if !(a < b) {
 		panic(fmt.Sprintf("Invalid range: %.2f ~ %.2f", a, b))
 	}
-	return a + Float64()*(b-a)
+	return a + r.Float64()*(b-a)
+}
+
+func Float64Range(a, b float64) float64 {
+	return Next().Float64Range(a, b)
 }
 
 // Float32 returns, as a float32, a pseudo-random number in [0.0,1.0).
-func Float32() float32 {
+func (r *Xor128Rand) Float32() float32 {
 	// Same rationale as in Float64: we want to preserve the Go 1 value
 	// stream except we want to fix it not to return 1.0
 	// There is a double rounding going on here, but the argument for
@@ -160,6 +214,11 @@ func Float32() float32 {
 		f = 0
 	}
 	return f
+}
+
+// Float32 returns, as a float32, a pseudo-random number in [0.0,1.0).
+func Float32() float32 {
+	return Next().Float32()
 }
 
 // Perm returns, as a slice of n ints, a pseudo-random permutation of the integers [0,n).
